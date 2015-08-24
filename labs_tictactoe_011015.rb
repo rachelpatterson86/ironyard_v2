fc44b1f75fe8df0eaaeced8f78c0c8ea24db6e91
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-08-24 11:10:11 -0400

    tic tac toe human v human and humvan v cpu completed

--- labs_tictactoe_011015.rb
+++ labs_tictactoe_011015.rb
@@ -1,200 +1,180 @@
-require 'pry'
-
-@game_options = {"1" => "Human v. Human", "2" => "Human v. Computer(Easy)", "3" => "Human v. Computer(Hard)"}
+@game_options = {1 => "Human v. Human", 2 => "Human v. Computer(Easy)", 3 => "Human v. Computer(Hard)"}
 @spaces = 1.upto(9).to_a
 @win = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-@game_mode
-@turn
+@turns = 9
 
-def greeting
-  puts "Welcome to Tic Tac Toe: The all American game!"
+def get_board                                 # output
+  grid_h =  "---+---+---\n"                   #    1 | 2 | 3
+  @spaces.each_slice(3) do |row|              #   ---+---+---
+    puts ' ' + row.join(' | ')                #    4 | 5 | 6
+    if row != @spaces.last(3)                 #   ---+---+---
+      puts grid_h                             #    7 | 8 | 9
+    end
+  end
+  puts "\n"
+end
+
+def validation(validate,input,validation_type,player=nil)
+  invalid_msg = " is not an option. Please select "
+  until validate
+    case validation_type
+    when 'game_mode'
+      puts "#{input}" + invalid_msg + "1, 2, or 3."
+      input = gets.chomp.to_i
+      validate = @game_options.has_key?(input)
+    when 'xo'
+      puts "#{input}" + invalid_msg + "'X' or 'O'."
+      input = gets.chomp
+      validate = input =~ /^[xo]$/i
+    when 'game_move'
+      puts "#{input}" + invalid_msg + "an available number on the board"
+      input = gets.chomp.to_i
+      validate = is_valid_move?(input)
+      puts "#{validate}"
+    end
+  end
+  validation_end(validation_type,input,player)
+end
+
+def validation_end(validation_type,input,player=nil)
+  case validation_type #separate into different function....
+  when 'game_mode'
+    puts "You've selected #{@game_options[input]} mode! Let's get started."
+    @game_mode = input
+  when 'xo'
+    @player1 = input.upcase!
+    @player1 == "X" ? @player2 = "O" : @player2 = "X"
+    puts "You are '#{@player1}'. Your oppenent is '#{@player2}'."
+  when 'game_move'
+    set_player_move(input, player)
+    set_win_arr(input, player)
+    get_board
+  end
 end
 
 def get_game_mode
-  puts "What Tic Tac Toe game would you like to play? Select 1, 2 or 3"
+  puts "Welcome to Tic Tac Toe: The all American game! \n What Tic Tac Toe game would you like to play? Select 1, 2 or 3"
   @game_options.each do |opt, msg|
     puts "#{opt} is #{msg}"
   end
-  user_input = gets.chomp
-
-  until @game_options.has_key?(user_input)
-    puts "#{user_input} is not an option. Please select 1, 2, or 3."
-    user_input = gets.chomp
-  end
-
-  puts "You've selected #{@game_options[user_input]} mode! Let's get started."
-
+  user_input = gets.chomp.to_i
+  validate = @game_options.has_key?(user_input)
+  validation(validate,user_input,"game_mode")
+  select_xo
 end
 
 def select_xo
-  puts "Select your character: 'X' or 'O'?"
+  puts "But first... do you want to be 'X' or 'O'?"
   user_input = gets.chomp
-  until player1 == /^[xo]$/i
-    puts "#{user_input} is not an option. Please select 'X' or 'O'."
-    user_input = gets.chomp
-  end
-  player1 = user_input.upcase
-  if player1 == "X" ? player2 = "O" : "X" end
-  puts "You are '#{player1}'. Your oppenent is '#{player2}'."
-end
-
-def print_rules
-  puts "To play, select any number for where you would like your piece to go. \n Let's begin!\n"
-end
-
-def get_board
-  grid_h =  "---+---+---\n"
-  @spaces.each_slice(3) do |row|
-    puts ' ' + row.join(' | ')
-    if row != @spaces.last(3)
-      puts grid_h
-    end
-  end
-
-# output
-#    1 | 2 | 3
-#   ---+---+---
-#    4 | 5 | 6
-#   ---+---+---
-#    7 | 8 | 9
-
- end
-
- def game_over?
-      if gameboard_full? || horizontal_win? || vertical_win? || diagonal_win?
-     return true
-      end
-   return false
-  end
-
- def gameboard_full?
-   unless @spaces.has_value?(" ")
-   return true
-  end
-   return false
- end
-
-def player_win?(player)
-end
-
- def get_player_move(is_player_1)
- #puts "Make your move!" + (is_player_1 ? "player 1." : "player 2.")
- player_move = gets.chomp.to_i
- until is_valid_move?(player_move) # && ! is_move_already_played?(player_move)
- puts "#{player_move} is not a valid option. Try again."
- player_move = gets.chomp
- end
- return player_move
- end
-
- def is_valid_move?(player_move)
- if player_move == 'A1' || player_move == 'A2' || player_move == 'A3'
- return true
-elsif player_move == 'B1' || player_move == 'B2' || player_move == 'B3'
- return true
-elsif player_move == 'C1' || player_move == 'C2' || player_move == 'C3'
- return true
- end
- return false
- end
-
-def is_move_already_played?(player_move)
- #is this space already taken by another player?
- if @spaces[player_move] == " "
- return false
- end
- return true
-end
-
- def set_player_move(player_move, player)
- if @spaces[player_move] == " "
-     @spaces[player_move] = player
-   else
-     puts "There is already a play in square #{player_move}. #{player} loses a turn."
-   end
-
- end
-
-def play_game_mode_1
-  player_1_turn = true
-
-  until game_over?
-    get_board
-    player_move = get_player_move(player_1_turn)
-    set_player_move(player_move, (player_1_turn ? 'X' : 'O'))
-    if player_1_turn
-      player_1_turn = false
-    else
-      player_1_turn = true
-    end #end if
-  end#end until
-
+  validate = user_input =~ /^[xo]$/i
+  validation(validate,user_input,"xo")
+  puts "Select any number on the board for where you would like to go! \n"
   get_board
-
-  if player_win?('X')
-    puts "Player 1 wins!"
-  elsif player_win?('O')
-    puts "Player 2 wins!"
-  elsif gameboard_full?
-    puts "It's a draw."
-  else
-    puts "How did this happen?!"
-  end
+  get_player_move(@player1)
 end
 
-def play_game_mode_2
-  human_turn = true
-
-  until game_over?
-    get_board
-    if human_turn
-      player_move = get_player_move(human_turn)
-      set_player_move(player_move, 'X')
-    else
-      player_move = generate_dumb_computer_move
-      set_player_move(player_move, 'O')
-    end
-
-    if human_turn
-      human_turn = false
-    else
-      human_turn = true
-    end #end if
-  end #end until
-
-  get_board
-
-  if player_win?('X')
-    puts "Human wins! (Of course)"
-  elsif player_win?('O')
-    puts "Computer wins! (Are you even trying, human?)"
-  elsif gameboard_full?
-    puts "It's a draw. (Come on, human. You can do better.)"
-  else
-    puts "How did this happen?!"
-  end
-
+def get_player_move(player)
+  player_move = gets.chomp.to_i
+  validate = is_valid_move?(player_move)
+#  puts "#{validate} from get player move"
+  validation(validate,player_move,'game_move',player)
 end
 
-  def generate_dumb_computer_move
-    #play the first available space
-    @spaces.each do |key, value|
-      if value == " "
-        return key
+  def is_valid_move?(player_move)
+    @spaces.any? {|s| s == player_move}
+  end
+
+  def set_player_move(player_move, player)
+    @spaces[player_move - 1] = player
+  end
+
+  def set_win_arr(player_move, player)
+    @win.each do |combo|
+      combo.each do |space|
+        if space == player_move
+          combo[combo.index(space)] = player
+        end
       end
     end
   end
+
+def player_win?
+  @win.any? {|w| w.uniq.length == 1}
 end
+
+def gameboard_full?
+  @turns.eql?(0)
+end
+
+def game_over?(player)
+  if @turns < 5 && player_win?
+    puts "#{player} wins! Thanks for playing."
+    return true
+  elsif gameboard_full?
+    puts "Board is full and it's a tie! Thanks for playing."
+    return true
+  end
+end
+
+def play_game_mode_1(player)
+  get_player_move(player)
+end
+
+def play_game_mode_2(player)
+  if player == @player1
+    get_player_move(player)
+  else
+    generate_dumb_computer_move(player)
+  end
+end
+
+  def generate_dumb_computer_move(player)      #play random value from @spaces
+    avail_comp_move = []
+    @spaces.each do |i|
+      if i.class == Fixnum
+        avail_comp_move << i
+      end
+    end
+    comp_move = avail_comp_move.sample
+    set_player_move(comp_move, player)
+  end
+
+#TODO play game mode 3 = minimax algorithm...
+# def play_game_mode_3(player)
+#   if player == @player1
+#     get_player_move(player)
+#   else
+#     generate_smart_computer_move
+#   end
+# end
+#
+#   def generate_smart_computer_move(player)      #play random value from @spaces
+#     avail_comp_move = []
+#     @spaces.each do |i|
+#       if i.class == Fixnum
+#         avail_comp_move << i
+#       end
+#     end
+#     comp_move = avail_comp_move.sample
+#     set_player_move(comp_move, player)
+#   end
 
 def tic_tac_toe
-  greeting
   get_game_mode
-  print_rules
-  if @game_mode == 1
-    play_game_mode_1
-  else
-    play_game_mode_2
+  player = @player1
+  @turns -= 1
+  until game_over?(player) do
+    @turns -= 1
+    player == @player1 ? player = @player2 : player = @player1
+      if @game_mode == 1
+        play_game_mode_1(player)
+      elsif @game_mode == 2
+        play_game_mode_2(player)
+      else
+        play_game_mode_3(player)
+      end
+    get_board
   end
-end#end tic_tac_toe
+end
 
 tic_tac_toe
